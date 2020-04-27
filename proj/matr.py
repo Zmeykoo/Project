73f51a84848b5eaad3d910b674d8a5231b497d58
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-04-27 17:46:27 +0300

    Add files via upload

--- proj/matr.py
+++ proj/matr.py
@@ -1,20 +1,27 @@
-#Матриця на 3 міста Львів, Київ, Черкаси
+#Матриця на 3 міста Львів, Київ, Черкаси,+ще одне місто для реал. другого етапу
 from math import inf
 import ffunc
 i=inf;n='\n'
 
-num=[[i,1,4],[8,i,2],[4,9,i]]
-new=[[],[],[]]
+num=[[i,1,4,8],[8,i,2,4],[4,9,i,6],[10,8,5,i]]
+new=[[],[],[],[]]
+new1=[[],[],[],[]]
 
+print('Origin:',n,num[0],n,num[1],n,num[2],n,num[3])
 mi=list(map(ffunc.line,num))#min line
+print(n,'Min numbers of line(рядок):',mi)
 
 
 ffunc.dell(num,mi,new)
+print(n,'after reducing lines',n,new[0],n,new[1],n,new[2],n,new[3])
+
 col=[min(x) for x in zip(*new)]#min column
+print(n,'Min num of column(стовпець)',col)
+ffunc.dell2(col,new1,new)
+
 
 SUMA=sum(mi)+sum(col)
+	
 
-print(n,num[0],n,num[1],n,num[2])	
-print(n,'Min numbers of line(рядок):',mi)
-print(n,'after reducing',n,new[0],n,new[1],n,new[2])
-print(n,'Min num of column(стовпець)',col,n,'Сума констант редукції:',SUMA)
+print(n,'after reducing columns',n,new1[0],n,new1[1],n,new1[2],n,new1[3],n,n)
+print(n,'Сума констант редукції:',SUMA)
